0ddeaad98b6dadb40e1a825d293086b6b9aeb8be
ywsyws <channing.platevoet@gmail.com>
2020-05-27 15:43:03 +0000

    used spider to get html

---
+++ nanny/nanny/spiders/nanny_spider.py
@@ -0,0 +1,20 @@
+import scrapy
+
+class NannySpider(scrapy.Spider):
+    name = "nannies"
+
+    def start_requests(self):
+        urls = [
+            # 'https://www.childcare.co.uk/find/Babysitters',
+            'https://www.childcare.co.uk/find/Babysitters/2',
+            'https://www.childcare.co.uk/find/Babysitters/3'
+        ]
+        for url in urls:
+            yield scrapy.Request(url=url, callback=self.parse)
+
+    def parse(self, response):
+        page = response.url.split("/")[-1]
+        filename = 'nannies-%s.html' % page
+        with open(filename, 'wb') as f:
+            f.write(response.body)
+        self.log('Saved file %s' % filename)
