ca5c9a5d5dfb4d037b731cc0b0ebec6e40246f47
ywsyws <channing.platevoet@gmail.com>
2020-05-29 23:37:34 +0200

    modified nanny_spider.py and created nannies.json

--- nanny/nanny/spiders/nanny_spider.py
+++ nanny/nanny/spiders/nanny_spider.py
@@ -4,38 +4,20 @@ class NannySpider(scrapy.Spider):
     name = "nannies"
     start_urls = [
         'https://www.childcare.co.uk/find/Babysitters',
-        'https://www.childcare.co.uk/find/Babysitters/2',
-        'https://www.childcare.co.uk/find/Babysitters/3'
+        # 'https://www.childcare.co.uk/find/Babysitters/2',
+        # 'https://www.childcare.co.uk/find/Babysitters/3'
     ]
 
     def parse(self, response):
-        # page = response.url.split("/")[-1]
-        # filename = 'nannies-%s.html' % page
-        # with open(filename, 'wb') as f:
-        #     f.write(response.body)
-        # self.log('Saved file %s' % filename)
-
         for nanny in response.css("div.results"):
             yield {
-                'name' : nanny.css('div.screen-name::text').get(),
-                'rating' : nanny.css('img.star-rating').re(r'\d\sstars')[0].split(' ')[0],
-                'hourly_rate' : nanny.css('div.flag::text').get(),
+                'name' : nanny.css('div.screen-name::text').getall(),
+                'rating' : nanny.css('img.star-rating').re(r'\d\s'),
+                'hourly_rate' : nanny.css('div.flag::text').re(r'\d*\.\d\d*')
             }
+        
+        next_page = response.css('a.view a::attr(href)').get()
+        if next_page is not None:
+            yield response.follow(next_page, callback=self.parse)
 
-
-
-        # response.css('div.screen-name').getall()
-        # response.css('img.star-rating').re(r'\d\sstars')
-        # response.css('img.star-rating').re(r'\d\sstars')[0].split(' ')[0]
-        # response.css('div.flag::text').getall()
-
-    #     nanny = response.css("div.results")[0]
-    #     name = nanny.css('div.screen-name::text').get()
-    #     rating = nanny.css('img.star-rating').re(r'\d\sstars')[0].split(' ')[0]
-    #     hourly_rate = nanny.css('div.flag::text').get()
-
-    #     for nanny in response.css("div.results")[0]:
-    #  ...:     name = nanny.css('div.screen-name::text').get()
-    #  ...:     rating = nanny.css('img.star-rating').re(r'\d\sstars')[0].split(' ')[0]
-    #  ...:     hourly_rate = nanny.css('div.flag::text').get()
-    #  ...:     print((dict(name=name, rating=rating, hourly_rate=hourly_rate)))
+        # stopped at "More examples and patterns" in "https://docs.scrapy.org/en/latest/intro/tutorial.html"
